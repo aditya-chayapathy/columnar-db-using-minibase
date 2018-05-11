f54c56679d3c743132e967fc08cf072750c889e7
Aditya <aditya-may9@users.noreply.github.com>
2018-05-10 20:03:09 -0700

    Adding new files

--- Phase 1/Code/javaminibase/src/iterator/Operand.java
+++
@@ -1,9 +0,0 @@
-package iterator;
-
-
-public class Operand {
-    public FldSpec symbol;
-    public String string;
-    public int integer;
-    public float real;
-}
--- Phase 2/Code/javaminibase/src/columnar/ValueClass.java
+++
@@ -1,6 +0,0 @@
-package columnar;
-
-public abstract class ValueClass<T> extends java.lang.Object{
-	protected T value;
-	public abstract T getValue();
-}
