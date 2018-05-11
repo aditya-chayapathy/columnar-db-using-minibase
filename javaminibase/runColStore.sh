b67954d159ca1cb75e4354ba913feef6421d7680
Aditya <aditya-may9@users.noreply.github.com>
2018-05-10 20:12:19 -0700

    Removing unnecessary files

--- javaminibase/runColStore.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/Desktop/PhaseII/Code/CSE510_DBMSI/javaminibase/
-
-mkdir -p outdemo
-javac  src/**/*.java -Xlint:unchecked -d outdemo
-cd outdemo
-cp ~/Desktop/sampledata1024.txt .
-cp ~/Desktop/sampledata.txt .
